51b5734ea08307695f4e6a37594fe5b0cc2f1e93
Huojiwang <21031211430@stu.xidian.edu.cn>
2023-07-07 12:14:04 +0800

    first

--- src/a.cpp
+++ src/a.cpp
@@ -1,50 +1,59 @@
 #include<iostream>
+#include<exception>
 // using namespace std;
 
 // using co_context::generator
 struct fib_frame{
+    bool ready = true;
+    std::exception_ptr exception;
     int a;
     int b;
     int c;
     int state;
-};
+    int param;
 
-int fib(fib_frame &f){
-    switch (f.state)
-    {
-    case /* constant-expression */ 0 :
-        f.state = 1;
-        goto s0;
-    case 1:
-        f.state = 2;
-        goto s1;
-    case 2:
-        f.state = 3;
-        goto s2;
-    case 3:
-        goto s3;
-s0:
-    f.a = f.b = 1;
-    return f.a;
-s1:
-    return f.b;
-s2:
-    for(;;)
-    {
-        /* code */
-        f.c = f.a + f.b;
-        return f.c;
-    s3:
-        f.a = f.b;
-        f.b = f.c;
+    fib_frame(int para):param(para){}
+    int resume(){
+        try{
+
+            switch (state)
+            {
+            case /* constant-expression */ 0 :
+                state = 1;
+                goto s0;
+            case 1:
+                state = 2;
+                goto s1;
+            case 2:
+                state = 3;
+                goto s2;
+            case 3:
+                goto s3;
+            }
+        s0:
+            a = b = 1;
+            return a;
+        s1:
+            return b;
+        s2:
+            for(;;){
+                    /* code */
+                    c = a + b;
+                    return c;
+                s3:
+                    a = b;
+                    b = c;
+            }
+        }catch (...) {
+            this->exception = std::current_exception();
+        }
     }
-    }
-}
+};
 int main(){
     using std::cout,std::endl;
-    fib_frame f;
+    fib_frame fib(3);
     for(int i = 0; i < 10;++i){
-        int x = fib(f);
+        int x = fib.resume();
         cout<<x<<endl;
     }
 }
